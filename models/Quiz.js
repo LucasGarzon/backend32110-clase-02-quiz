b40d4979f2359486ce359683716bc55e324b1593
Lucas Garzon <garzon.lucas.m@gmail.com>
2022-08-15 14:27:13 -0300

    feat: change name method continue

--- models/Quiz.js
+++ models/Quiz.js
@@ -11,7 +11,7 @@ export class Quiz {
     return this.questions[this.questionIndex]
   }
 
-  continue(answer) {
+  validateAndContinue(answer) {
     if (this.getCurrentQuestion().isCorrectAnswer(answer)) {
       this.score++
     }
